7104aac24492bbec8b43c3f85d2e45e962d1c5c9
Kyle Williams <williams.kyleg@gmail.com>
2020-01-04 10:54:57 -0500

    Working on update items feature.

--- scripts/main.js
+++ scripts/main.js
@@ -5,14 +5,74 @@ const addButton = document.getElementById('add-button');
 addButton.addEventListener('click', () => {
     const li = document.createElement('li');
     const groceryItem = document.getElementById('grocery-item').value;
-    const text = document.createTextNode(groceryItem);
+    const text = document.createElement('span');
+    const textClass = document.createAttribute('class');
+    textClass.value = 'text-element';
+    text.setAttributeNode(textClass);
+    const textContent = document.createTextNode(`${groceryItem}`)
+    text.appendChild(textContent);
     li.appendChild(text);
+
     if (groceryItem === '') {
         alert('Enter a grocery item.');
     } else {
         document.getElementById('my-ul').appendChild(li);
     }
-    //Creates the X box and attaches it to the list item.
+
+    //Gets li element items needed for text entry updating.
+    let updatedList = document.getElementsByTagName('li');
+    
+    //Removes old text and replaces it with an empty input.
+    for (let i = 0; i < updatedList.length; i++) {
+        updatedList[i].firstChild.onclick = () => {
+            updatedList[i].removeChild(updatedList[i].firstChild);
+            const newElement = document.createElement('input');
+            const newAttribute = document.createAttribute('type');
+            newAttribute.value = 'text';
+            newElement.setAttributeNode(newAttribute);
+            const newAttribute2 = document.createAttribute('class');
+            newAttribute2.value = 'updated-items';
+            newElement.setAttributeNode(newAttribute2);
+
+            //Insert the new input element at the beginning of the proper <li> element.
+            updatedList[i].insertBefore(newElement, updatedList[i].firstChild);
+
+            //Takes input from newly created text inputs and replaces them with updated text.
+                //Creates and updates list of text inputs.
+                const inputList = document.getElementsByClassName('updated-items');
+                console.log(inputList);
+
+                for (let j = 0; j < inputList.length; j++) {
+                    inputList[j].onkeydown = () => {
+                        if (event.key === 'Enter') {
+                            const updatedVar = inputList[j].value;
+                            inputList[j].parentNode.removeChild(inputList[j]);
+                            const updatedText = document.createElement('span');
+                            const updatedTextClass = document.createAttribute('class');
+                            updatedTextClass.value = 'text-element';
+                            updatedText.setAttributeNode(updatedTextClass);
+                            console.log(inputList[j]);
+                            updatedText.textContent = updatedVar;
+                            updatedList[j].insertBefore(updatedText, updatedList[j].firstChild);
+                        }
+                    }
+                }
+                
+            
+        }
+    }
+    
+
+    //Creates the quantity input for grocery item.
+    const quantity = document.createElement('input');
+    quantity.setAttribute('class', 'quantity');
+    quantity.setAttribute('type', 'number');
+    quantity.setAttribute('step', '1');
+    quantity.setAttribute('min', '0');
+    li.appendChild(quantity);
+
+    //Creates the X box and attaches it to the list item. (EDIT: The span will now need to be a child of 
+    //the number input for quantity of grocery item.
     const span = document.createElement('span');
     const txt = document.createTextNode(' X');
     span.className = 'close';
@@ -28,4 +88,13 @@ addButton.addEventListener('click', () => {
             x.style.display = 'none';
         }
     }
+
+    //Update existing grocery item names.
+    //const updateList = text + text;
+
+    //for (let i = 0; i < updateList.length; i++) {
+      //  console.log('updateList loop');
+    //}
+    //console.log(text);
+    
 });
